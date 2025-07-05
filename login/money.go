0cd5b617f4d201e4cbbf6539e596cd2a88c1a40c
mingozz <dcrazymingo@gmail.com>
2025-07-05 12:30:58 +0800

    本地分支commit后，尝试更新main代码再rebase

--- login/money.go
+++ login/money.go
@@ -23,8 +23,7 @@ main分支修改远端代码
 main分支修改远端代码
 main分支修改远端代码
 main分支修改远端代码
-main分支修改远端代码
-main分支修改远端代码
-main分支修改远端代码
-main分支修改远端代码
+2025/07/05
+本地分支修改代码  commit后，尝试在main分支更新代码rebase再push
+本地分支修改代码  commit后，尝试在main分支更新代码rebase再push
 */
